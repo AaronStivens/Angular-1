66387d4b8d52f88b4e6c0be19610c84e2a318e79
Aaron Hernandez <aaronhernandez14963@gmail.com>
2023-03-11 00:39:07 -0600

    Comentarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Component } from '@angular/core';
-import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faFilm } from '@fortawesome/free-solid-svg-icons';
-import { faFish } from '@fortawesome/free-solid-svg-icons';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import 'hammerjs';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatIconModule} from '@angular/material/icon';
-import {MatRippleModule} from '@angular/material/core';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; //Modulo de animaciones
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';//Modulo de iconos font awesome
+import { faFilm } from '@fortawesome/free-solid-svg-icons';//Modulo de iconos Fafilm
+import { faFish } from '@fortawesome/free-solid-svg-icons';//Modulo de iconos Fafilm
+import {MatToolbarModule} from '@angular/material/toolbar';//Modulo de barra de herramientas
+import 'hammerjs'; //Modulo hammerjs para interacciones tactiles
+import { FlexLayoutModule } from '@angular/flex-layout';//modulo que simplifica la creacion de animaciones mas flexibles
+import {MatIconModule} from '@angular/material/icon';//Modulo de iconos de angular
+import {MatRippleModule} from '@angular/material/core';//Modulo que brinda animacion a botones al hacer click sobre ellos
+import { MatMenuModule } from '@angular/material/menu';//Modulo para menus desplegables
+import { MatSidenavModule } from '@angular/material/sidenav';//Modulo para crear menus deslizantes
+import { MatListModule } from '@angular/material/list';//Modulos para crear listas
 
 
 @NgModule({
@@ -40,7 +39,7 @@ import { MatListModule } from '@angular/material/list';
 })
 
 export class AppModule {
-  constructor(library: FaIconLibrary) {
+  constructor(library: FaIconLibrary) {//Importacion de los iconos Fa y Fish
     library.addIcons(faFilm,faFish);
   }
  
