d08d06ad2c4b81eb382c0182a7b1f6f415200e79
Aaron Hernandez <aaronhernandez14963@gmail.com>
2023-03-11 22:31:55 -0600

    Ventana emergenete 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,12 +17,23 @@ export class AppComponent {
   constructor(public dialog: MatDialog) {}
 
   openDialog() {
-    this.dialog.open(Direccion1,);
+    this.dialog.open(Direccion1);
   }
-  
+
+openDialog3() {
+    this.dialog.open(Blog);
+  }
+
+  openDialog2(){
+    this.dialog.open(Contacto, {
+      width: '600px',
+      
+    });
+  }
 }
+
 @Component({
-  selector: 'dialog-data-example-dialog',
+  selector: 'app-root',
   templateUrl: 'Direccion.html',
 })
 
@@ -30,6 +41,30 @@ export class Direccion1 {
   constructor(public dialogRef: MatDialogRef<Direccion1>) {}
   close() {
     this.dialogRef.close();
+  }
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'Contacto.html',
+})
+
+export class Contacto { 
+  constructor(public dialogRef2: MatDialogRef<Contacto>) {}
+  close() {
+    this.dialogRef2.close();
+  }
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'Blog.html',
+})
+
+export class Blog { 
+  constructor(public dialogRef3: MatDialogRef<Blog>) {}
+  close() {
+    this.dialogRef3.close();
   }
 }
 
